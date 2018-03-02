cc6fb142a7c1b4597d28e022fb57d59de91b2250
Dotan Nahum <jondotan@gmail.com>
2018-03-02 21:26:06 +0200

    build: tweaks.

--- scripts/standalone.js
+++ scripts/standalone.js
@@ -27,30 +27,32 @@ const main = async () => {
 
   console.log('standalone: done.')
   console.log((await execa.shell(`ls ${wd}`)).stdout)
-  if (process.env.PUBLISH_BREWTAP) {
-    const matches = (await execa.shell(
-      `shasum ${wd}/hygen.macos.v${v}.tar.gz`
-    )).stdout.match(/([a-f0-9]+)\s+/)
-    console.log(matches)
-    if (matches && matches.length > 1) {
-      const sha = matches[1]
-      const cmds = [
-        `cd /tmp`,
-        `git clone git://${repo} brew-tap`,
-        `cd brew-tap`,
-        `cat '${brewFormula(sha, v)}' > hygen.rb`,
-        `git config user.email jondotan@gmail.com`,
-        `git config user.name 'Dotan Nahum'`,
-        `git add .`,
-        `git commit -m 'hygen: auto-release'`,
-        `git push "https://${
-          process.env.GITHUB_TOKEN
-        }@${repo}" master > /dev/null 2>&1`
-      ]
-      for (const cmd of cmds) {
-        await exec.shell(cmd)
-      }
+
+  console.log('standalone: publishing to homebrew tap...')
+  const matches = (await execa.shell(
+    `shasum ${wd}/hygen.macos.v${v}.tar.gz`
+  )).stdout.match(/([a-f0-9]+)\s+/)
+  console.log(matches)
+  if (matches && matches.length > 1) {
+    const sha = matches[1]
+    const cmds = [
+      `cd /tmp`,
+      `git clone git://${repo} brew-tap`,
+      `cd brew-tap`,
+      `cat '${brewFormula(sha, v)}' > hygen.rb`,
+      `git config user.email jondotan@gmail.com`,
+      `git config user.name 'Dotan Nahum'`,
+      `git add .`,
+      `git commit -m 'hygen: auto-release'`,
+      `git push "https://${
+        process.env.GITHUB_TOKEN
+      }@${repo}" master > /dev/null 2>&1`
+    ]
+    for (const cmd of cmds) {
+      await exec.shell(cmd)
     }
+
+    console.log('standalone: publish done.')
   }
 }
 
