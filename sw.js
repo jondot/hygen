b7458d693fd2d3d001310c6a2482cd7cbe648074
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-13 13:07:23 +0000

    Deploy jondot/hygen to github.com/jondot/hygen.git:gh-pages

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-001d19debacc667722ae.js","21119f5aae5397bf722da9bf723017b6"],["/commons-c746cb2799ca82f8d161.js","0a97fc5f95fe022d05b5714ecc8a534b"],["/index.html","89be6b04b155f8365697de7b59fa32e9"],["/manifest.json","671a68832bca77112328e6ea070a4bb7"],["/offline-plugin-app-shell-fallback/index.html","21d33cc98ab1c6b429ec94ab88c002be"]];
+var precacheConfig = [["/app-001d19debacc667722ae.js","21119f5aae5397bf722da9bf723017b6"],["/commons-c746cb2799ca82f8d161.js","0a97fc5f95fe022d05b5714ecc8a534b"],["/index.html","0491ebd3dff27e6b3781fc840b9e57d6"],["/manifest.json","671a68832bca77112328e6ea070a4bb7"],["/offline-plugin-app-shell-fallback/index.html","081f5525ae8a9f3db74dc7cd32f640f8"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
