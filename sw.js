7f8c0f5dbcaf465cfdc1c8c190d6aabe4584a7a8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-26 17:17:32 +0000

    Deploy jondot/hygen to github.com/jondot/hygen.git:gh-pages

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-001d19debacc667722ae.js","d17a7c86ff13709c1a20af03543791ca"],["/commons-3d31cbc66a91ddd20a09.js","fa258233386ecee57103b7689e55be6f"],["/index.html","47d216de94fc979cfa030eabc96daf4b"],["/manifest.json","671a68832bca77112328e6ea070a4bb7"],["/offline-plugin-app-shell-fallback/index.html","6ac1e3986a8ea60484704abe8311a019"]];
+var precacheConfig = [["/app-001d19debacc667722ae.js","d17a7c86ff13709c1a20af03543791ca"],["/commons-3d31cbc66a91ddd20a09.js","fa258233386ecee57103b7689e55be6f"],["/index.html","c9b37034dc05346d9c32058336fde1a5"],["/manifest.json","671a68832bca77112328e6ea070a4bb7"],["/offline-plugin-app-shell-fallback/index.html","08906b45c306d79bffb10972505a169d"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
@@ -45,7 +45,7 @@ var ignoreUrlParametersMatching = [/^utm_/];
 
 
 
-var addDirectoryIndex = function (originalUrl, index) {
+var addDirectoryIndex = function(originalUrl, index) {
     var url = new URL(originalUrl);
     if (url.pathname.slice(-1) === '/') {
       url.pathname += index;
@@ -53,7 +53,7 @@ var addDirectoryIndex = function (originalUrl, index) {
     return url.toString();
   };
 
-var cleanResponse = function (originalResponse) {
+var cleanResponse = function(originalResponse) {
     // If this is not a redirected response, then we don't have to do anything.
     if (!originalResponse.redirected) {
       return Promise.resolve(originalResponse);
@@ -75,7 +75,7 @@ var cleanResponse = function (originalResponse) {
     });
   };
 
-var createCacheKey = function (originalUrl, paramName, paramValue,
+var createCacheKey = function(originalUrl, paramName, paramValue,
                            dontCacheBustUrlsMatching) {
     // Create a new URL object to avoid modifying originalUrl.
     var url = new URL(originalUrl);
@@ -91,7 +91,7 @@ var createCacheKey = function (originalUrl, paramName, paramValue,
     return url.toString();
   };
 
-var isPathWhitelisted = function (whitelist, absoluteUrlString) {
+var isPathWhitelisted = function(whitelist, absoluteUrlString) {
     // If the whitelist is empty, then consider all URLs to be whitelisted.
     if (whitelist.length === 0) {
       return true;
@@ -104,7 +104,7 @@ var isPathWhitelisted = function (whitelist, absoluteUrlString) {
     });
   };
 
-var stripIgnoredUrlParameters = function (originalUrl,
+var stripIgnoredUrlParameters = function(originalUrl,
     ignoreUrlParametersMatching) {
     var url = new URL(originalUrl);
     // Remove the hash; see https://github.com/GoogleChrome/sw-precache/issues/290
