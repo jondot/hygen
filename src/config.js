2169331056757f1991045fab6d60c95ee7ea842c
Scott M Parrish <anithri@gmail.com>
2019-06-18 09:08:43 -0600

    add higher order functions to replace lodash

--- src/config.js
+++ src/config.js
@@ -2,24 +2,22 @@
 import type { ResolverIO } from './types'
 import importedPath from 'path'
 
-import L from 'lodash'
-// inline fp methods due to perf
-const reduce = (f, init) => arr => L.reduce(arr, f, init)
+const reduce = (f, init) => arr => arr.reduce(f, init)
+const uniq = arr => arr.filter((elem, pos) => arr.indexOf(elem) === pos)
+const map = f => arr => arr.map(f)
 
-const reversePathsToWalk = L.flow(
-  ({ folder, path }) => ({ resolved: path.resolve(folder), path }),
-  ({ resolved, path }) => ({ parts: resolved.split(path.sep), path }),
-  ({ parts, path }) => {
-    return reduce((acc, p) => [...acc, path.join(L.last(acc), p)], [
-      path.join(L.head(parts), path.sep)
-    ])(L.tail(parts))
-  },
-  L.reverse,
-  L.uniq
-)
+const reversePathsToWalk = ({ folder, path }) => {
+  const resolved = path.resolve(folder)
+  const parts = resolved.split(path.sep)
+
+  const all = reduce((acc, p) => [...acc, path.join(acc.slice(-1)[0], p)], [
+    path.join(parts[0], path.sep),
+  ])(parts.slice(1)).reverse()
+  return uniq(all)
+}
 
 const configLookup = (file: string, folder: string, path: any = importedPath) =>
-  L.map(reversePathsToWalk({ folder, path }), p => path.join(p, file))
+  map(p => path.join(p, file))(reversePathsToWalk({folder, path}))
 
 class ConfigResolver {
   configFile: string
