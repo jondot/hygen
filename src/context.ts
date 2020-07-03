4781e4c0f4208e4972acd41bdc2753a2206dc49b
Dotan Nahum <jondotan@gmail.com>
2020-07-03 14:48:47 +0300

    fix: name variable should be PascalCase #219

--- src/context.ts
+++ src/context.ts
@@ -1,40 +1,57 @@
+import { RunnerConfig } from './types'
 
-import { RunnerConfig } from "./types";
+const inflection = require('inflection')
+const changeCase = require('change-case')
 
-const inflection = require('inflection');
-const changeCase = require('change-case');
-
+const localsToCapitalize = ['name']
+const localsDefaults = {
+  name: 'unnamed',
+}
 // supports kebab-case to KebabCase
-inflection.undasherize = str => str.split(/[-_]/).map(w => w[0].toUpperCase() + w.slice(1).toLowerCase()).join('');
+inflection.undasherize = str =>
+  str
+    .split(/[-_]/)
+    .map(w => w[0].toUpperCase() + w.slice(1).toLowerCase())
+    .join('')
 
 const helpers = {
   capitalize(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    return string.charAt(0).toUpperCase() + string.slice(1)
   },
   inflection,
-  changeCase
-};
+  changeCase,
+}
 
 const doCapitalization = (hsh, [key, value]) => {
-  hsh[key] = value;
+  hsh[key] = value
 
-  if (localsToCapitalize.includes(key)) hsh[helpers.capitalize(key)] = helpers.capitalize(value);
+  if (localsToCapitalize.includes(key))
+    hsh[helpers.capitalize(key)] = helpers.capitalize(value)
 
-  return hsh;
-};
+  return hsh
+}
 
-const localsToCapitalize = ['name'];
-const localsDefaults = {
-  name: 'unnamed'
-};
-
-const capitalizedLocals = (locals: any) => Object.entries(locals).reduce(doCapitalization, {});
+const capitalizedLocals = (locals: any) =>
+  Object.entries(locals).reduce(doCapitalization, {})
 
 const context = (locals: any, config: RunnerConfig = {}) => {
-  const localsWithDefaults = Object.assign({}, localsDefaults, config.localsDefaults, locals);
-  const configHelpers = config && (typeof config.helpers === "function" ? config.helpers(locals, config) : config.helpers) || {};
-  return Object.assign(localsWithDefaults, capitalizedLocals(localsWithDefaults), {
-    h: { ...helpers, ...configHelpers }
-  });
-};
-module.exports = context;
+  const localsWithDefaults = {
+    ...localsDefaults,
+    ...config.localsDefaults,
+    ...locals,
+  }
+  const configHelpers =
+    (config &&
+      (typeof config.helpers === 'function'
+        ? config.helpers(locals, config)
+        : config.helpers)) ||
+    {}
+  return Object.assign(
+    localsWithDefaults,
+    capitalizedLocals(localsWithDefaults),
+    {
+      h: { ...helpers, ...configHelpers },
+    },
+  )
+}
+module.exports = context
